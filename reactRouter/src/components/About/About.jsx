abe63c51d39db85f7e7ed89143a689e021b71f4a
AftabMankapure <aftabmankapure@gmail.com>
2024-11-14 16:31:03 +0530

    about component created

---
+++ reactRouter/src/components/About/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+          <div className="md:5/12 lg:w-5/12">
+            <img
+              src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-20165.jpg?t=st=1731568888~exp=1731572488~hmac=05cb18bcc3ebd0ffe6c24bc4e131a64efc4c0ff8ad3b87c208e1e026a498c73b&w=740 "
+              className="w-90"
+              alt="image"
+            />
+          </div>
+          <div className="md:7/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+              React development is carried out by passionate developers
+            </h2>
+            <p className="mt-6 text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
+              omnis voluptatem accusantium nemo perspiciatis delectus atque
+              autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
+              consequatur! Officiis id consequatur atque doloremque!
+            </p>
+            <p className="mt-4 text-gray-600">
+              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+              expedita at? Asperiores nemo possimus nesciunt dicta veniam
+              aspernatur quam mollitia.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
