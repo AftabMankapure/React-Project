3f0027796325312ba80fa4a3de896203e73f4079
AftabMankapure <aftabmankapure@gmail.com>
2024-11-19 11:35:48 +0530

    context api

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+
+import UserContextProvider from "./context/UserContextProvider";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
-    <>
+    <UserContextProvider>
       <h1> React context </h1>
-    </>
-  )
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
