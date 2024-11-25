2d573123a94864682ed78ce1fdb92fd7711706bf
AftabMankapure <aftabmankapure@gmail.com>
2024-11-25 14:51:40 +0530

    theme switcher

--- themeSwitcherToggle/src/App.jsx
+++ themeSwitcherToggle/src/App.jsx
@@ -1,9 +1,39 @@
+import Card from "./components/Card";
+import ThemeBtn from "./components/Themebtn";
+import { ThemeProvider } from "./contexts/theme";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-green-400">
-        Hello world!
-      </h1>{" "}
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
